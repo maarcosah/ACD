b3813fc2d4dcff3f56befb290849495c790bda84
Marcos Álvarez <149399722+maarcosah@users.noreply.github.com>
2025-12-04 21:04:16 +0100

    UPD: jwt bean userDetails

--- practica1/src/main/java/com/persistance/practica1/config/SecurityConfig.java
+++ practica1/src/main/java/com/persistance/practica1/config/SecurityConfig.java
@@ -7,8 +7,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.persistance.practica1.filter.JwtRequestFilter;
@@ -42,5 +46,27 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("usuario")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN", "USER")
+                .build();
+        UserDetails moderator = User.builder()
+                .username("moderator")
+                .password(passwordEncoder().encode("asdf"))
+                .roles("MODERATOR", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin, moderator);
     }
 }
